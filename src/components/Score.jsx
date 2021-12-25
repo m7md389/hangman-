0c92efab9e6bed0961a14ec9e6069e2fea1253a3
Mohammad <m7md389@gmail.com>
2021-12-25 02:15:58 +0200

    rename jsx

---
+++ src/components/Score.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+export class Score extends Component {
+  getClassName() {
+    const otherClasses = "score";
+
+    if (this.props.score >= 80) return otherClasses + " high-score";
+
+    if (this.props.score >= 50) return otherClasses + " medium-score";
+
+    return otherClasses + " low-score";
+  }
+
+  render() {
+    return <div className={this.getClassName()}>{this.props.score}</div>;
+  }
+}
+
+export default Score;
